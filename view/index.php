db6494e147b98101af63d8f052a378e26dbf8981
Juan David <brittoargote789@gmail.com>
2018-04-30 03:22:08 +0000

    New login completed

--- view/index.php
+++ view/index.php
@@ -1,3 +1,55 @@
-<?php   
 
-phpinfo();
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="../css/styles.css" type="text/css" />
+    <title>PDO BRITTO</title>
+</head>
+<body>
+   
+   <div class="container formulario">
+                <div class="row">
+                        <div class="col-xs-4 col-xs-offset-4">
+                            <img src="../img/img.png"  class="center-block"></img>
+                        </div>
+                        
+                </div>
+                
+                <div class="espacio">
+                </div>
+                
+                <div class="row">
+                    <fieldset class="col-xs-10 col-xs-offset-1">
+                        <legend class="hidden-xs"><h3>Inicio de Sesìon</h3></legend>
+                        
+                        <form class="form horinzotal">
+                            
+                            <div class="form-group">
+                                <label class="col-xs-12" for="usuario"><h4>User</h4></label>
+                                <div class="col-xs-10 col-xs-offset-1">
+                                    <input type="text" name="" id="usuario" class="form-control formulario_input"/>
+                                </div>
+                            </div>
+                            
+                            <div class="form-group">
+                                <label class="col-xs-12" for="contra"><h4>Password</h4></label>
+                                <div class="col-xs-10 col-xs-offset-1">
+                                    <input type="text" name="" id="contra" class="form-control formulario_input"/>
+                                </div>
+                            </div>
+                
+                            <div class="form-group">
+                                <button type="button" class="btn btn-primary center-block">Aceptar</button>
+                            </div>
+                            
+                        </form>
+                    </fieldset>
+                </div>
+    </div>
+    
+</body>
+</html>
